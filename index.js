2036350d780ce77803712eb74215cd553704fab1
Antonella Jotayan <78166670+Antonella-Jotayan@users.noreply.github.com>
2022-06-21 22:38:00 -0300

    agregar items a ls & mostrarlo

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 $(document).on("pagecreate", function () {});
+
+function removeItem() {}
+
+function editItem() {}
+
+$(document).on("ready", function () {
+  showTodos();
+
+  $("#saveTodo").on("click", function addItem() {
+    let inputText = $("#inputText").val();
+    addItemToLocalStorage(inputText);
+    showTodos();
+    $.mobile.navigate("#home");
+    $("#inputText").val("");
+  });
+
+  $("#cancel").on("click", function () {
+    $("#inputText").val("");
+    $.mobile.navigate("#home");
+  });
+});
+
+function addItemToLocalStorage(inputText) {
+  let todos = getItemFromLocalStorage();
+  if (!todos) {
+    let items = [];
+    items.push(inputText);
+    localStorage.setItem("todos", toJson(items));
+    return;
+  }
+  todos.push(inputText);
+  localStorage.setItem("todos", toJson(todos));
+}
+
+function toJson(arr) {
+  return JSON.stringify(arr);
+}
+
+function getItemFromLocalStorage() {
+  let todos = window.localStorage.getItem("todos");
+  if (!todos) return;
+  return JSON.parse(todos);
+}
+
+function showTodos() {
+  let todos = getItemFromLocalStorage();
+  console.log(todos);
+  if (!todos) {
+    return $("#todos").append(
+      "<div><p style='color:white'>No hay ningun todo!</p></div>"
+    );
+  }
+  $("#todos").empty();
+  $.each(todos, function (index, value) {
+    $("#todos").append(`<div><p style='color:white'>${value}</p></div>`);
+  });
+}
